9311db186c305ddd7d622dc19f1f58328a5822c4
Cowboy Lynk <cowboylynk@icloud.com>
2017-04-26 14:14:06 -0400

    Super Basic UI

--- TheWordGame/TheWordGame/ViewController.swift
+++ TheWordGame/TheWordGame/ViewController.swift
@@ -9,6 +9,20 @@
 import UIKit
 
 class ViewController: UIViewController {
+    // Outelts
+    @IBOutlet weak var wordLabel: UILabel!
+    @IBOutlet weak var userInput: UITextField!
+    
+    // Actions
+    @IBAction func playWord(_ sender: Any) {
+        updateWord()
+    }
+    
+    // Functions
+    func updateWord() {
+        wordLabel.text = userInput.text
+    }
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
