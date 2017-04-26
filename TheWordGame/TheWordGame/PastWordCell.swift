99e370dc089b69c793da79743e539967d024a8d2
Daniel <d.mccrystal512@gmail.com>
2017-04-26 15:26:48 -0400

    started UI

---
+++ TheWordGame/TheWordGame/PastWordCell.swift
@@ -0,0 +1,24 @@
+//
+//  PastWordCell.swift
+//  TheWordGame
+//
+//  Created by Daniel McCrystal on 4/26/17.
+//  Copyright © 2017 Lampshade Software. All rights reserved.
+//
+
+import UIKit
+
+class PastWordCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
