3e9614a34b13f1aa2602c88030615b5294cb7ba5
Cowboy Lynk <cowboylynk@icloud.com>
2017-04-26 15:13:31 -0400

    Removed changes to ViewController

--- TheWordGame/TheWordGame/ViewController.swift
+++ TheWordGame/TheWordGame/ViewController.swift
@@ -10,19 +10,10 @@ import UIKit
 
 class ViewController: UIViewController {
     // Outelts
-    @IBOutlet weak var wordLabel: UILabel!
-    @IBOutlet weak var userInput: UITextField!
     
     // Actions
-    @IBAction func playWord(_ sender: Any) {
-        updateWord()
-    }
     
-    // Functions
-    func updateWord() {
-        wordLabel.text = userInput.text
-    }
-    
+    // Functions    
 
     override func viewDidLoad() {
         super.viewDidLoad()
