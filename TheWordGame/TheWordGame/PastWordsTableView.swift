cd8017f79f3ba6cdf7a401ef02c23f4d08549dd3
Daniel <d.mccrystal512@gmail.com>
2017-04-27 14:34:00 -0400

    added necessary files

---
+++ TheWordGame/TheWordGame/PastWordsTableView.swift
@@ -0,0 +1,21 @@
+//
+//  PastWordsTableView.swift
+//  TheWordGame
+//
+//  Created by Daniel McCrystal on 4/27/17.
+//  Copyright © 2017 Lampshade Software. All rights reserved.
+//
+
+import UIKit
+
+class PastWordsTableView: UITableView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
